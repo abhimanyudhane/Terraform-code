5cc2ef3fee3f6f525b571341c2870ddf5ecec141
Abhimanyu dhane <96540561+abhimanyudhane@users.noreply.github.com>
2022-04-09 22:06:58 +0530

    Update backend.tf

--- terraform_workspace-demo/backend.tf
+++ terraform_workspace-demo/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "ctl-terraform-state-ap-south-1"
+    bucket         = "backend-bucket-1"
     key            = "tfdemo/workspace"
     region         = "ap-south-1"
-    dynamodb_table = "terraform-state"
+    dynamodb_table = "backend-db-table"
   }
 }
